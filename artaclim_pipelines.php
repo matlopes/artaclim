eb135c3d1a051b508afdcc470bae98742bc5c6f3
matlopes <mathieu.lopes@gmail.com>
2020-08-10 09:35:26 +0200

    Ajout d'un champ extra pour sélectionner le guide utilisateur

--- artaclim_pipelines.php
+++ artaclim_pipelines.php
@@ -5,4 +5,34 @@ function artaclim_jqueryui_plugins($scripts){
    return $scripts;
 } 
 
+function artaclim_declarer_champs_extras($champs = array()) {
+
+	
+	// Table : spip_documents
+	if (!is_array($champs['spip_documents'])) {
+		$champs['spip_documents'] = array();
+	}
+
+	$champs['spip_documents']['guide'] = array(
+			'saisie' => 'case',
+			'options' => array(
+				'nom' => 'guide',
+				'label' => 'Guide utilisateur',
+				'explication' => 'Cochez cette case pour indiquer que le document est le guide utilisateur',
+				'valeur_oui' => 'on',
+				'restrictions' => array(
+					'modifier' => array(
+						'auteur' => 'admin',
+					),
+				),
+				'sql' => 'varchar(3) DEFAULT \'\' NOT NULL',
+				'rechercher_ponderation' => '2',
+			),
+			'verifier' => array(
+			),
+		);
+
+	return $champs;
+}
+
 ?>
